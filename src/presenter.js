5cd9bead769f3e33fe6e19b54c458c035c52b970
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-08-20 17:46:36 -0400

    Added enter price input

--- src/presenter.js
+++ src/presenter.js
@@ -1,13 +1,16 @@
 
 
 const quant = document.querySelector("#quantity");
-const form = document.querySelector("#quantity-form");
+const prices = document.querySelector("#price")
+const form = document.querySelector("#totalizer-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const price = Number.parseInt(prices.value);
   const quantity = Number.parseInt(quant.value);
 
-  div.innerHTML = "<p>" + quantity + "</p>";
+
+  div.innerHTML = "<p>" + `Actual quantity ${quantity} ` + `Price ${price}` + "</p>";
 });
