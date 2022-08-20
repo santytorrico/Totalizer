15c6a2b15a44c7798412e383995c10871ded9f77
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-08-20 18:31:50 -0400

    Added tax input

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,7 @@
 import netprice from "./calculate";
 
 const quant = document.querySelector("#quantity");
+const taxes = document.querySelector("#tax");
 const prices = document.querySelector("#price")
 const form = document.querySelector("#totalizer-form");
 const div = document.querySelector("#resultado-div");
@@ -10,7 +11,7 @@ form.addEventListener("submit", (event) => {
 
   const price = Number.parseInt(prices.value);
   const quantity = Number.parseInt(quant.value);
+  const tax = Number.parseInt(taxes.value);
 
-
-  div.innerHTML = "<p>" + `The net Price is ${netprice(price,quantity)}` + "</p>";
+  div.innerHTML = "<p>" + `The net Price is ${netprice(price,quantity)} `+ `Taxes applied to this product ${tax}%` + "</p>";
 });
